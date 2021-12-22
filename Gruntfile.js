6d92abcd61564f966fda4d80cd3f6724b259308f
oushihao <oushihao97@163.com>
2021-12-22 13:29:12 +0800

    style: 格式化代码

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,21 @@
 module.exports = function (grunt) {
-	// Project configuration.
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		uglify: {
-			options: {
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-			},
-			build: {
-				src: 'lib/<%= pkg.name %>.js',
-				dest: 'dist/<%= pkg.name %>.min.js'
-			}
-		}
-	});
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'lib/<%= pkg.name %>.js',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    }
+  });
 
-	// 加载包含 "uglify" 任务的插件。
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+  // 加载包含 "uglify" 任务的插件。
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	// 默认被执行的任务列表。
-	grunt.registerTask('default', ['uglify']);
+  // 默认被执行的任务列表。
+  grunt.registerTask('default', ['uglify']);
 };
